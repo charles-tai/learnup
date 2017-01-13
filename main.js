2850e5d69e04bd595f6f9c1008c9992b05e40b27
Charles Tai <cwtai86@gmail.com>
2017-01-13 15:20:17 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+var App = {};
+App.init = function () {
+
+  var render = function (state) {
+    var locations = state.locations;
+    var locationsTemplate = Handlebars.compile($('#locations-template').html());
+    Handlebars.registerPartial('location', $('#location-template').html());
+    Handlebars.registerHelper('each', function(context, options) {
+      var ret = "";
+      for(var i=0, j=context.length; i<j; i++) {
+        ret = ret + options.fn(context[i]);
+      }
+      return ret;
+    });
+    var locationsHTML = locationsTemplate({ locations });
+    $("#locations").html(locationsHTML);
+    $('.select-location').on('click', function (event) {
+      var button = $(event.target)
+      var locationId = button.data('location-id');
+      selectLocation(locationId);
+    })
+
+  }
+  var selectLocation = function (locationId) {
+    var data = JSON.stringify({ location_id: locationId, id: 1 });
+    $.ajax({
+       url: 'https://location-winnow.herokuapp.com/users/1',
+       type: 'PUT',
+       data: data,
+       dataType: 'application/json',
+       success: function(response) {
+         console.log('response', response);
+       }
+     })
+  }
+  var retrieveLocations = function (query) {
+    var url_encoded_search_string = encodeURI(query);
+    $.getJSON('https://location-winnow.herokuapp.com/locations?query=' + url_encoded_search_string, (response) => {
+      locations = response;
+      render({locations})
+    });
+  }
+
+
+  $('#search-field').on('keyup', function (event) {
+      var target = event.target.value;
+      // search trie for target
+      retrieveLocations(target);
+  })
+
+}
